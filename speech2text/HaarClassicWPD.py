ddcede679ac3323a4b3a52e78571f22bf3b6164f
Richard Werner <rwerner@profinit.eu>
2020-05-24 13:26:02 +0000

    Time optimizations

--- speech2text/HaarClassicWPD.py
+++ speech2text/HaarClassicWPD.py
@@ -1,17 +1,19 @@
 import tensorflow as tf
 
 class HaarClassicWPD:
-    def __init__(self, signal):
-        self.signal = signal
-        self.max_level = tf.cast(tf.math.floor(self.__log2(tf.size(signal, out_type=tf.float16))), tf.int32)
-        self.tree = {'0' : tf.reshape(signal, [1, tf.size(signal)])}
-
+    @staticmethod
     def __log2(self, x):
         numerator = tf.math.log(x)
         denominator = tf.math.log(tf.constant(2, dtype=numerator.dtype))
         return numerator / denominator
 
-    def __high_pass_filter(self, signal):
+    @staticmethod
+    def __both_filters(signal):
+        even_odds = tf.transpose(tf.reshape(signal, [tf.shape(signal)[0]//2, 2]))
+        return tf.stack([tf.math.subtract(even_odds[0], even_odds[1]), tf.reduce_sum(even_odds, 0)], 0)/2
+
+    @staticmethod
+    def __high_pass_filter(signal):
         return tf.math.divide(
                    tf.reduce_sum(
                        tf.transpose(
@@ -20,7 +22,8 @@ class HaarClassicWPD:
                    ), 2
                )
 
-    def __low_pass_filter(self, signal):
+    @staticmethod
+    def __low_pass_filter(signal):
         return tf.math.divide(
                    tf.math.subtract(
                        tf.transpose(
@@ -30,20 +33,22 @@ class HaarClassicWPD:
                     ), 2
                 )
 
-    def __sig_to_feature(self, signal):
+    @staticmethod
+    def __sig_to_feature(signal):
         return tf.reduce_logsumexp(signal)
 
-    def get_level(self, level):
-        curr_level=1
+    @staticmethod
+    def get_level(signal, level):
+        signal = tf.reshape(signal, [1, tf.size(signal)])
+        curr_level = 1
         while curr_level <= level:
-            if str(curr_level) in self.tree:
-                curr_level += 1
-                continue
-
-            self.tree[str(curr_level)] = tf.map_fn(self.__low_pass_filter, self.tree[str(curr_level - 1)])
-            self.tree[str(curr_level)] = tf.concat([self.tree[str(curr_level)], tf.map_fn(self.__high_pass_filter, self.tree[str(curr_level - 1)])], 0)
+            signal = tf.map_fn(HaarClassicWPD.__both_filters, signal)
+            sig_shape = tf.shape(signal)
+            signal = tf.reshape(signal, [sig_shape[0]*sig_shape[1], sig_shape[2]])
             curr_level += 1
-        return self.tree[str(level)]
+        return signal
 
-    def get_features_level(self, level):
-        return tf.map_fn(self.__sig_to_feature, self.get_level(level))
+    @staticmethod
+    def get_features_level(signal, level):
+        return tf.map_fn(HaarClassicWPD.__sig_to_feature, HaarClassicWPD.get_level(signal, level))
+
